6a8bc7dc1f488e044c3a977b793f79a65bb06c94
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-07-30 18:28:36 +0300

    teacher's version on 30.07

--- src/main/java/telran/net/TcpClientServerSession.java
+++ src/main/java/telran/net/TcpClientServerSession.java
@@ -3,12 +3,15 @@ package telran.net;
 import java.net.*;
 import java.io.*;
 
-public class TcpClientServerSession {
+public class TcpClientServerSession extends Thread{
 	Socket socket;
 	Protocol protocol;
 
 	public TcpClientServerSession(Socket socket, Protocol protocol) {
 		this.socket = socket;
+		//TODO
+		//using the method setSoTimeout and some solution for getting session to know about shutdown
+		//you should stop the thread after shutdown command
 		this.protocol = protocol;
 	}
 	public void run() {
@@ -16,10 +19,16 @@ public class TcpClientServerSession {
 				new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintStream sender = new PrintStream(socket.getOutputStream())){
 			String line = null;
+			//FIXME 
+			//figure out solution for exiting from the thread after shutdown
 			while((line = receiver.readLine()) != null) {
 				String responseStr = protocol.getResponseWithJSON(line);
 				sender.println(responseStr);
 			}
+			//TODO handling SocketTimeoutException for exiting from the thread on two conditions
+			//1. Shutdown has been performed
+			//2. Thread exists in IDLE state more than 1 minute
+			//exiting from the cycle should be followed by closing connection
 			
 		} catch (Exception e) {
 			System.out.println(e);
