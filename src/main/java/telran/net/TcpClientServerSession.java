cfc66a154e8af53d42e3a4bbc422e77d53029030
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-07-31 19:03:42 +0300

    changes TcpClientSrverSession: adds timeout for socket and ends thread after  shutdown

--- src/main/java/telran/net/TcpClientServerSession.java
+++ src/main/java/telran/net/TcpClientServerSession.java
@@ -3,36 +3,43 @@ package telran.net;
 import java.net.*;
 import java.io.*;
 
-public class TcpClientServerSession extends Thread{
-	Socket socket;
-	Protocol protocol;
+public class TcpClientServerSession extends Thread {
+    Socket socket;
+    Protocol protocol;
+    boolean running = true;
 
-	public TcpClientServerSession(Socket socket, Protocol protocol) {
-		this.socket = socket;
-		//TODO
-		//using the method setSoTimeout and some solution for getting session to know about shutdown
-		//you should stop the thread after shutdown command
-		this.protocol = protocol;
-	}
-	public void run() {
-		try (BufferedReader receiver =
-				new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				PrintStream sender = new PrintStream(socket.getOutputStream())){
-			String line = null;
-			//FIXME 
-			//figure out solution for exiting from the thread after shutdown
-			while((line = receiver.readLine()) != null) {
-				String responseStr = protocol.getResponseWithJSON(line);
-				sender.println(responseStr);
-			}
-			//TODO handling SocketTimeoutException for exiting from the thread on two conditions
-			//1. Shutdown has been performed
-			//2. Thread exists in IDLE state more than 1 minute
-			//exiting from the cycle should be followed by closing connection
-			
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
+    public TcpClientServerSession(Socket socket, Protocol protocol) {
+        this.socket = socket;
+        this.protocol = protocol;
+        try {
+            socket.setSoTimeout(60000);
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public void shutdown() {
+        running = false;
+    }
+
+    public void run() {
+        try (Socket socketResource = socket;
+             BufferedReader receiver = new BufferedReader(new InputStreamReader(socketResource.getInputStream()));
+             PrintStream sender = new PrintStream(socketResource.getOutputStream())) {
+
+            String line = null;
+            while (running && (line = receiver.readLine()) != null) {
+                String responseStr = protocol.getResponseWithJSON(line);
+                sender.println(responseStr);
+                if ("shutdown".equalsIgnoreCase(line.trim())) {
+                    shutdown();
+                }
+            }
+        } catch (SocketTimeoutException e) {
+            
+            System.out.println("Client do not responding. Connection closed");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
